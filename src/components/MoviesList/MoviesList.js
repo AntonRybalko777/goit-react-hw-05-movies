4305ae746efceadc3f503071c50a0fc1ef94d7c8
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-10-29 21:55:37 +0200

    starting styles

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,15 +1,25 @@
 import { Link, useLocation } from 'react-router-dom';
-
+const defaultImg =
+  'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
 export const MoviesList = ({ movies }) => {
   const location = useLocation();
 
   return (
     <div>
       <ul>
-        {movies.map(({ id, title, name, release_date }) => (
+        {movies.map(({ id, title, name, poster_path }) => (
           <li key={id}>
             <Link to={`/movies/${id}`} state={{ from: location }}>
-              {title ?? name} ({release_date && release_date.split('-')[0]})
+              <img
+                src={
+                  poster_path
+                    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                    : defaultImg
+                }
+                alt="poster"
+                width={150}
+              />
+              {title ?? name}
             </Link>
           </li>
         ))}
